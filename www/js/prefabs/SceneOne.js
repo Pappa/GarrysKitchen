36dcac9f98808392f82ae358b029f8bfda34538d
Robert Hill <dr.pappa@gmail.com>
2015-11-06 15:57:34 +0000

    Fire

--- www/js/prefabs/SceneOne.js
+++ www/js/prefabs/SceneOne.js
@@ -6,9 +6,37 @@ K.SceneOne = function(game, x, y) {
   this.game = game;
 
   this.createBackground();
+  this.createOven();
+  this.fire = this.game.make.sprite(240, 29, 'fire');
+  this.fire.animations.add('moving', [0, 1, 2, 3, 4], 15, true);
+  this.addChild(this.fire);
+  this.fire.play('moving');
 };
 
 K.util.extend(K.SceneOne, Phaser.Sprite);
 
 K.SceneOne.prototype.createBackground = function () {
+  var background = this.game.make.sprite(0, 0, 'lightBlue');
+  background.width = 540;
+  background.height = 88;
+  this.addChild(background);
+  var line = this.game.make.sprite(0, 88, 'black');
+  line.width = 540;
+  line.height = 2;
+  this.addChild(line);
+};
+
+K.SceneOne.prototype.createOven = function () {
+  var ovenTop = this.game.make.sprite(200, 23, 'peach');
+  ovenTop.width = 170;
+  ovenTop.height = 3;
+  this.addChild(ovenTop);
+  var ovenPart1 = this.game.make.sprite(200, 26, 'darkRed');
+  ovenPart1.width = 170;
+  ovenPart1.height = 3;
+  this.addChild(ovenPart1);
+  var ovenPart2 = this.game.make.sprite(200, 29, 'darkRed');
+  ovenPart2.width = 115;
+  ovenPart2.height = 20;
+  this.addChild(ovenPart2);
 };
--- www/js/states/Preload.js
+++ www/js/states/Preload.js
@@ -4,11 +4,14 @@ K.PreloadState = {
   preload: function() {
     this.load.image('grey', 'assets/images/grey.png');
     this.load.image('white', 'assets/images/white.png');
-    this.load.image('logo', 'assets/images/logo.png');
+    this.load.image('black', 'assets/images/black.png');
+    this.load.image('peach', 'assets/images/peach.png');
     this.load.image('lightBlue', 'assets/images/lightBlue.png');
     this.load.image('darkBlue', 'assets/images/darkBlue.png');
+    this.load.image('darkRed', 'assets/images/darkRed.png');
+    this.load.image('logo', 'assets/images/logo.png');
     //this.load.audio('splat', ['assets/audio/splat.mp3', 'assets/audio/splat.ogg']);
-    //this.load.spritesheet('player', 'assets/images/player_spritesheet.png', 180, 405, 3, 0, 0);
+    this.load.spritesheet('fire', 'assets/images/fire.png', 30, 17, 5, 0, 1);
     //this.load.spritesheet('colours', 'assets/images/colours.png', 180, 405, 3, 0, 0);
   },
   create: function() {
